35d5d33e03f9d488115d1d18b2a34a9fcbe84297
Tak <tak@clearsumm.it>
2015-09-24 18:14:44 +0700

    add content

---
+++ example/calc.js
@@ -0,0 +1,22 @@
+﻿exports.plus = function (a, b) {
+    return a + b;
+}
+
+exports.minus = function (a, b) {
+    return a - b;
+}
+
+module.exports.multiply = function (a, b) {
+    return a * b;
+}
+
+module.exports.divide = function (a, b) {
+    return doDivide(a, b);
+}
+
+function doDivide(a, b) {
+    return a / b
+}
+
+//exports = 'destroy';
+//module.exports = 'destroy';
---
+++ example/math.js
@@ -0,0 +1,8 @@
+﻿var math = require("./calc.js");
+
+console.log(math.plus(1, 2));
+console.log(math.minus(2, 1));
+console.log(math.multiply(2, 2));
+console.log(math.divide(10, 2));
+//console.log(math.doDivide(10, 2));
+//console.log(doDivide(10, 2));
