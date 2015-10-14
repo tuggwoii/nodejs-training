4955fb1e45e91a0aee8b6ca710043f0af436e170
Tak <tugg.solo@gmail.com>
2015-10-14 19:43:47 +0700

    web socket

--- example/ch3/upload/upload.js
+++ example/ch3/upload/upload.js
@@ -1,39 +1,41 @@
-var formidable = require('formidable'),
-    http = require('http'),
+var formidable = require('formidable');
+var app = require('http').createServer(handler);
     util = require('util'),
     fs = require('fs');
 
-http.createServer(function(req, res) {
-  if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
-    // parse a file upload
-    var form = new formidable.IncomingForm();
-    form.uploadDir = "./dir";
-    form.keepExtensions = true;
-    form.parse(req, function(err, fields, files) {
-      res.writeHead(200, {'content-type': 'text/plain'});
-      res.write('received upload:\n\n');
-      res.end(util.inspect({fields: fields, files: files}));
-    });
-    form.on('file', function(field, file) {
-        fs.rename(file.path, form.uploadDir + "/" + file.name);
-    });
-    form.on('progress', function(bytesReceived, bytesExpected) {
-        console.log(bytesReceived + '/' + bytesExpected);
-    });
+var io = require('socket.io')(app);
+var fs = require('fs');
 
-    return;
-  }
 
-  // show a file upload form
-  res.writeHead(200, {'content-type': 'text/html'});
-  res.end(
-    '<form action="/upload" enctype="multipart/form-data" method="post">'+
-    '<input type="text" name="title"><br>'+
-    '<input type="file" name="upload" multiple="multiple"><br>'+
-    '<input type="submit" value="Upload">'+
-    '</form>'
-  );
+function handler (req, res) { 
+     if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+        var form = new formidable.IncomingForm();
+        form.uploadDir = "./dir";
+        form.keepExtensions = true;
+        form.parse(req, function(err, fields, files) {
+          res.writeHead(200, {'content-type': 'text/plain'});
+          res.write('received upload:\n\n');
+          res.end(util.inspect({fields: fields, files: files}));
+        });
+        form.on('file', function(field, file) {
+            fs.rename(file.path, form.uploadDir + "/" + file.name);
+        });
+        form.on('progress', function(bytesReceived, bytesExpected) {
+            console.log(form.name +':'+ bytesReceived + '/' + bytesExpected);
+            io.sockets.in('sessionId').emit('uploadProgress', (bytesReceived * 100) / bytesExpected);
+        });
 
-}).listen(5001, function () {
-    console.log('Server run on localhost:5001');   
+        return;
+    }
+    else {
+        res.writeHead(200);
+        res.end('');
+    }
+    
+}
+app.listen(5001);
+
+io.on('connection', function (socket) {
+    console.log('connect to socket');
+    socket.join('sessionId');
 });
