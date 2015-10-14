a4883030e977aa53fc866c44d25ee6c7d979991a
Tak <tugg.solo@gmail.com>
2015-10-14 23:06:04 +0700

    update web socket code

--- example/ch3/upload/upload.js
+++ example/ch3/upload/upload.js
@@ -8,20 +8,33 @@ var fs = require('fs');
 
 
 function handler (req, res) { 
-     if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
+     if (req.method === 'OPTIONS') {
+          var headers = {};
+          // IE8 does not allow domains to be specified, just the *
+          // headers["Access-Control-Allow-Origin"] = req.headers.origin;
+          headers["Access-Control-Allow-Origin"] = "*";
+          headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+          headers["Access-Control-Allow-Credentials"] = false;
+          headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+          headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+          res.writeHead(200, headers);
+          res.end();
+     }
+     else if (req.url == '/upload' && req.method.toLowerCase() == 'post') {
         var form = new formidable.IncomingForm();
-        form.uploadDir = "./dir";
+        form.uploadDir = "/Projects/Umbraco 7.3/Umbraco 7.3/media/upload";
         form.keepExtensions = true;
         form.parse(req, function(err, fields, files) {
-          res.writeHead(200, {'content-type': 'text/plain'});
-          res.write('received upload:\n\n');
-          res.end(util.inspect({fields: fields, files: files}));
+          var headers = {};
+          headers["content-type"] = "application/json";
+          headers["Access-Control-Allow-Origin"] = "*";
+          res.writeHead(200, headers);
+          res.end('{"success": true}');
         });
         form.on('file', function(field, file) {
             fs.rename(file.path, form.uploadDir + "/" + file.name);
         });
         form.on('progress', function(bytesReceived, bytesExpected) {
-            console.log(form.name +':'+ bytesReceived + '/' + bytesExpected);
             io.sockets.in('sessionId').emit('uploadProgress', (bytesReceived * 100) / bytesExpected);
         });
 
